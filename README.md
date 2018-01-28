1f3e94047ab6b6aaab2804dc67059eb2a1f0a08b
Lambert Tran <lb.tran648@gmail.com>
2018-01-27 22:08:08 -0800

    refactor code and add readme file

--- README.md
+++ README.md
@@ -2,3 +2,24 @@
 #### Front-End can be found [here](https://github.com/LambertTran/reactjs-auto27-web)
 #### This server was built on Nodejs with a CMS that allows admin user to manage their car inventory and customer images.
 #### You can check you the live version at [link](http://ec2-52-52-42-92.us-west-1.compute.amazonaws.com:8080/auto27)
+
+#### If you want to run this project locally, you need to add AWS S3 Credentials for it to work properly
+
+```
+|-- identity
+    |-- aws.js
+```
+
+**First**, you need to create a folder called **identity**
+
+**Then**, create a JS file **aws.js** where you define your AWS S3 credentials as following
+
+```
+const awsCredentials = {
+  secretAccessKey: your-secret-access-key,
+  accessKeyId: your-access-key-ID,
+}
+module.exports = awsCredentials
+```
+
+
---
+++ aws/upload-aws.js
@@ -0,0 +1,48 @@
+/** =================================
+                Packages
+**==================================*/
+const AWS = require('aws-sdk');
+const multer = require('multer');
+const multerS3  = require('multer-s3');
+const awsCredentials = require('../identity/aws');
+
+/** =================================
+                Body
+**==================================*/
+
+/** config s3 */
+AWS.config.update({
+  secretAccessKey: awsCredentials.secretAccessKey,
+  accessKeyId:awsCredentials.accessKeyId,
+  region: 'us-east-1'
+})
+
+
+var s3 = new AWS.S3();
+
+/** handle upload images */
+var upload = multer({
+    storage: multerS3({
+        s3: s3,
+        bucket: 'images-storage-cardealer',
+        contentType: multerS3.AUTO_CONTENT_TYPE ,
+        key: function (req, file, cb) {
+            var name = Date.now().toString() + file.originalname;
+            cb(null, name );
+        }
+    })
+});
+
+var uploadCustomer = multer({
+    storage: multerS3({
+        s3: s3,
+        bucket: 'customer-images-car-dealer',
+        contentType: multerS3.AUTO_CONTENT_TYPE ,
+        key: function (req, file, cb) {
+            var name = Date.now().toString() + file.originalname;
+            cb(null, name );
+        }
+    })
+});
+
+module.exports = {upload,uploadCustomer};
